267621414363cefe18f61151a183b279f44771d0
patern0ster <karasakal99@gmail.com>
2020-09-26 17:38:44 +0300

    Final

---
+++ djangonautic/urls.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+from articles import views as article_view
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('articles/', include('articles.urls')),
+    path('about', views.about),
+    path('accounts/', include('accounts.urls')),
+    path('', article_view.article_list,name="home"),
+
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, documet_root=settings.MEDIA_ROOT)
